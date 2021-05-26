d4b51e097b58a08b61c8dcab37e1283304c2de32
Osman Yıldız <osmanyildiz046@gmail.com>
2021-05-26 23:42:50 +0300

    ListViewController has been created.

---
+++ TravelBook/TravelBook/ListViewViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ListViewViewController.swift
+//  TravelBook
+//
+//  Created by Osman Yıldız on 26.05.2021.
+//
+
+import UIKit
+
+class ListViewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+
+}
